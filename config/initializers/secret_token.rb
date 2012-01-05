8676eff8e347eb5b8259261b5a09c524f9f4ef97
Matt CR <mclr@ymail.com>
2012-01-05 13:08:27 -0800

    beginning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lion::Application.config.secret_token = '6a3a011260fa0a6f993753e4b4eb935455580bcbde71e6b4a12ddc6742fe6b89b298952ba9ce125cd2ef8d5b5169b3b2eb8ee48f877a34378c03e7202479f5f9'
